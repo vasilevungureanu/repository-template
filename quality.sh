489545abb022b9a2e049846f7bdd2d4666789228
Vasile Ungureanu <vasile.ungureanu@titanium-soft.com>
2019-08-22 10:21:36 +0300

    Add quality script

---
+++ quality.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+#
+# Run static code analysis tools to validate Shell code.
+
+# If a command fails then do not proceed and fail this script too
+set -o errexit
+set -o pipefail
+
+#######################################
+# Find all shell scripts and check them
+# Globals:
+#  None
+# Arguments:
+#  None
+# Returns:
+#  None
+#######################################
+checkAllShellScripts(){
+# shellcheck disable=SC2044
+for file in $(find . -type f \( -name "*.sh" \));
+  do shellcheck "${file}";
+done;
+}
+
+# Validate Shell code with shellcheck
+checkAllShellScripts
