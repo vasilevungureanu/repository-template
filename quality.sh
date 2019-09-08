b7b987510f21eff5e3a5de708f7447aae6c25a22
Vasile Ungureanu <vasile.v.ungureanu@gmail.com>
2019-09-08 15:01:50 +0300

    Update quality.sh

--- quality.sh
+++ quality.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Run static code analysis tools to validate Shell code.
+# Runs the static code analysis tools to validate Shell code.
 
 # If a command fails then do not proceed and fail this script too
 set -o errexit
